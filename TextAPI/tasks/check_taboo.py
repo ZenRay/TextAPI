cbe5ec4137df8c1a994e93457520f805a383ded5
ZenRay <jake2011ren@gmail.com>
2020-05-06 14:09:52 +0800

    update docstring   commmit 71ca6c8

--- TextAPI/tasks/check_taboo.py
+++ TextAPI/tasks/check_taboo.py
@@ -5,14 +5,6 @@ This module provides guard whether the text contains taboo words. Jieba
 segment is the basis:
 * Firstly, separate the word from the text
 * Secondly, check whether the word is taboo
-those found in Perl.  It supports both 8-bit and Unicode strings; both
-the pattern and the strings being processed can contain null bytes and
-characters outside the US ASCII range.
-
-Regular expressions can contain both special and ordinary characters.
-Most ordinary characters, like "A", "a", or "0", are the simplest
-regular expressions; they simply match themselves.  You can
-concatenate ordinary characters, so last matches the string 'last'
 """
 from __future__ import absolute_import
 
